c52f757f01441e607866c907753641a62f6cdec0
dikeke <dikeke@qiniu.com>
2025-12-10 10:10:21 +0800

    pybullet

--- src/pybullet/check_joints.py
+++ src/pybullet/check_joints.py
@@ -6,7 +6,8 @@ import time
 # 【重要】请将下面的路径替换为你自己的 URDF 文件绝对路径或相对路径
 # 如果找不到文件，脚本会使用 PyBullet 自带的 r2d2 机器人作为演示
 # 例如: urdf_path = "./lerobot/robots/my_robot.urdf"
-urdf_path = "r2d2.urdf" 
+#urdf_path = "r2d2.urdf" 
+urdf_path = "../urdf/RJ2506-20251125-all-13-std.urdf"
 # ===========================================
 
 def main():
